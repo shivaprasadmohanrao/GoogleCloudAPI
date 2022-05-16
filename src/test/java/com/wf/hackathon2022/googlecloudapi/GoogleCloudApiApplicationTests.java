7d766cf05b070e60b7dee083d72a74892345e6cd
shivaprasadmohanrao <79432422+shivaprasadmohanrao@users.noreply.github.com>
2022-05-17 01:38:56 +0530

    Update GoogleCloudApiApplicationTests.java

--- src/test/java/com/wf/hackathon2022/googlecloudapi/GoogleCloudApiApplicationTests.java
+++ src/test/java/com/wf/hackathon2022/googlecloudapi/GoogleCloudApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.wf.hackathon2022.googlecloudapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+
 class GoogleCloudApiApplicationTests {
-
+	
+	@MockBean
+	private SpringApplication sa;
+	
+	
 	@Test
 	void contextLoads() {
 	}
+	
+//	@Test
+//	public void main() {
+//		Mockito.when(sa.run(Mockito.any(String.class))).thenReturn(null);
+//		GoogleCloudApiApplication.main(new String[] {});
+//	}
 
 }
